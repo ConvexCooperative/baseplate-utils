d57383eb5a6333e1725ffa68952d57bf734ec347
Joel Denning <joeldenning@gmail.com>
2022-03-15 12:53:45 -0600

    Fix file extensions (#3)
    * Fix file extensions
    
    * Add changeset
    
    * Remove unnecessary tsconfig property

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,15 @@
 /**  @type {import('@jest/types').Config.ProjectConfig} */
 const config = {
   transform: {
-    "\\.[jt]sx?$": "babel-jest",
+    "\\.[jt]sx?$": "ts-jest",
+  },
+  globals: {
+    "ts-jest": {
+      useESM: true,
+    },
+  },
+  moduleNameMapper: {
+    "(.+)\\.js": "$1",
   },
   extensionsToTreatAsEsm: [".ts"],
 };
