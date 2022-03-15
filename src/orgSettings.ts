c53241b3f678b0bc726e36504ac24c8b1e6529bd
Joel Denning <joeldenning@gmail.com>
2022-03-15 12:11:52 -0600

    Initial implementation (#1)

---
+++ src/orgSettings.ts
@@ -0,0 +1,65 @@
+import { merge } from "lodash-es";
+
+const defaultSettings: OrgSettings = {
+  staticFiles: {
+    microfrontendProxy: {
+      environments: {},
+    },
+  },
+  importMapCacheControl: "public, must-revalidate, max-age=60",
+  cors: {
+    allowOrigins: ["*"],
+    exposeHeaders: [],
+    // 1 day in seconds
+    maxAge: 86400,
+    allowCredentials: true,
+    allowHeaders: [],
+    allowMethods: ["GET", "HEAD", "OPTIONS"],
+  },
+  orgExists: false,
+};
+
+export function mergeDefaultOrgSettings(
+  orgSettings: RecursivePartial<OrgSettings>
+): OrgSettings {
+  const finalSettings: OrgSettings = merge({}, defaultSettings, orgSettings);
+  return finalSettings;
+}
+
+interface StaticFileSettings {
+  microfrontendProxy: {
+    environments: {
+      [key: string]: StaticFileProxySettings;
+    };
+  };
+}
+
+export interface StaticFileProxySettings {
+  useFoundryHosting: boolean;
+  customHost: string;
+}
+
+export interface OrgSettings {
+  staticFiles: StaticFileSettings;
+  importMapCacheControl: string;
+  cors: CORSSettings;
+  orgExists: boolean;
+}
+
+export interface CORSSettings {
+  allowOrigins: string[];
+  exposeHeaders: string[];
+  maxAge: number;
+  allowCredentials: boolean;
+  allowMethods: string[];
+  allowHeaders: string[];
+}
+
+// https://stackoverflow.com/questions/41980195/recursive-partialt-in-typescript
+type RecursivePartial<T> = {
+  [P in keyof T]?: T[P] extends (infer U)[]
+    ? RecursivePartial<U>[]
+    : T[P] extends object
+    ? RecursivePartial<T[P]>
+    : T[P];
+};
